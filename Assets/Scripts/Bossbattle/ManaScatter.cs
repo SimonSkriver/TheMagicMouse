cdeb0af9cf97d5d2ac27e7e7bb6685b18017f3e8
Rune <runestenn@gmail.com>
2025-11-20 10:37:17 +0100

    ManaScatter for Bossbattle

---
+++ Assets/Scripts/Bossbattle/ManaScatter.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ManaScatter : MonoBehaviour
+{
+    [SerializeField] protected GameObject manaPrefab;
+    [SerializeField] protected int count = 10;
+    [SerializeField] protected Vector2 areaSize = new Vector2(25f, 15f);
+    [SerializeField] protected AnimationCurve verticalDensity; 
+
+    
+    void Start()
+    {
+        ScatterMana();
+    }
+    
+    void ScatterMana()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            float yNorm = Random.value;
+            if (verticalDensity != null && verticalDensity.Evaluate(yNorm) < Random.value)
+                continue;
+
+            Vector2 spawnPos = new Vector2(Random.Range(-areaSize.x / 2f, areaSize.x / 2f), Mathf.Lerp(-areaSize.y / 2f, areaSize.y / 2f, yNorm));
+
+            Instantiate(manaPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
